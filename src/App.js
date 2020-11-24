21f8ce1c615755a73b19e7b81362d312a431fb93
Sagar <acharyasaagar@gmail.com>
2020-11-24 11:04:44 +0200

    next commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
+  const [projects, setProjects] = React.useState(null)
+  React.useEffect(() => {
+    const fetchMsg = async () => {
+      const res = await fetch(process.env.REACT_APP_API_URL)
+      const projects = await res.json()
+      setProjects(projects)
+    }
+    fetchMsg()
+  })
+  const ulStyle = {
+    listStyle: 'none',
+    width: '350px',
+    margin: '32px auto',
+  }
+  const listItemStyle = {
+    background: '#FFECFF',
+    color: '#002278',
+    fontFamily: 'Arial',
+    padding: '24px 32px',
+    margin: '4px',
+    borderRadius: '4px',
+  }
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h2>Todos </h2>
+      <ul style={ulStyle}>
+        {projects?.map((project) => (
+          <li style={listItemStyle} key={project.id}>
+            <h4>{project.title}</h4>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
